b8c439c5fb8afb4f7711dd946a8b6e08877902a9
mandy <mandy.yuan19@gmail.com>
2026-01-24 16:18:07 -0800

    setpoints for shooting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CombinedTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CombinedTeleOp.java
@@ -36,19 +36,23 @@ public class CombinedTeleOp extends OpMode {
     private ElapsedTime timer;
     private double runtime;
 
+    private final Pose farBlue = new Pose(70,25,120);
+    private final Pose farRed = new Pose(72,25,60);
+    private final Pose nearBlue = new Pose(72,82,135);
+    private final Pose nearRed = new Pose(72,82,45);
+
 
 
     @Override
     public void init() {
         follower = Constants.createFollower(hardwareMap);
-//        follower.setStartingPose(startingPose == null ? new Pose(72, 72, 0) : startingPose);
+        follower.setStartingPose(startingPose == null ? new Pose(72, 72, 0) : startingPose);
         follower.update();
         telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
-
-        pathChain = () -> follower.pathBuilder() //Lazy Curve Generation
-                .addPath(new Path(new BezierLine(follower::getPose, new Pose(72, 72))))
-                .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(45), 0.8))
-                .build();
+//        pathChain = () -> follower.pathBuilder() //Lazy Curve Generation
+//                .addPath(new Path(new BezierLine(follower::getPose, new Pose(72, 72))))
+//                .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(45), 0.8))
+//                .build();
         intakeMotor = hardwareMap.get(DcMotorEx.class, "intake");
         shooterMotor1 = hardwareMap.get(DcMotorEx.class, "shooter1");
         shooterMotor2 = hardwareMap.get(DcMotorEx.class, "shooter2");
@@ -68,7 +72,6 @@ public class CombinedTeleOp extends OpMode {
 
     @Override
     public void loop() {
-        //Call this once per loop
         follower.update();
         telemetryM.update();
 
@@ -80,19 +83,39 @@ public class CombinedTeleOp extends OpMode {
             follower.setPose(new Pose(72, 72, 180));
         }
 
-        if (!automatedDrive) {
-            //Make the last parameter false for field-centric
-            //In case the drivers want to use a "slowMode" you can scale the vectors
+        //Shooting setpoints (far/small triangle and close/large triangle)
+        if (gamepad1.xWasPressed() && !automatedDrive) {
+            automatedDrive = true;
+            driveToNearBlue();
+        }
+        else if (gamepad2.yWasPressed()&&!automatedDrive) {
+            automatedDrive = true;
+            driveToNearRed();
+        }
+        else if (gamepad2.right_trigger>0&&!automatedDrive){
+            automatedDrive = true;
+            driveToFarBlue();
+        }
+        else if (gamepad2.right_bumper&&!automatedDrive){
+            automatedDrive = true;
+            driveToFarRed();
+        }
 
-            //This is the normal version to use in the TeleOp
+        if (automatedDrive && !follower.isBusy()){
+            automatedDrive = false;
+            follower.startTeleopDrive();
+        }
+
+
+        //Normal Teleop driving
+        if (!automatedDrive) {
             if (!slowMode) follower.setTeleOpDrive(
                     -gamepad1.left_stick_y * 0.8,
                     -gamepad1.left_stick_x * 0.8,
                     -gamepad1.right_stick_x * 0.8,
                     false // Robot Centric
             );
-
-                //This is how it looks with slowMode on
+            //SlowMode on
             else follower.setTeleOpDrive(
                     -gamepad1.left_stick_y * slowModeMultiplier,
                     -gamepad1.left_stick_x * slowModeMultiplier,
@@ -101,35 +124,6 @@ public class CombinedTeleOp extends OpMode {
             );
         }
 
-//        //Automated PathFollowing
-//        if (gamepad1.aWasPressed()) {
-//            follower.followPath(pathChain.get());
-//            automatedDrive = true;
-//        }
-//        //Stop automated following if the follower is done
-//        if (automatedDrive && (gamepad1.bWasPressed() || !follower.isBusy())) {
-//            follower.startTeleopDrive();
-//            automatedDrive = false;
-//        }
-
-//        //Slow Mode
-//        if (gamepad1.rightBumperWasPressed()) {
-//            slowMode = !slowMode;
-//        }
-//
-//        //Optional way to change slow mode strength
-//        if (gamepad1.xWasPressed()) {
-//            slowModeMultiplier += 0.25;
-//        }
-//
-//        //Optional way to change slow mode strength
-//        if (gamepad2.yWasPressed()) {
-//            slowModeMultiplier -= 0.25;
-//        }
-
-        telemetryM.debug("position", follower.getPose());
-        telemetryM.debug("velocity", follower.getVelocity());
-        telemetryM.debug("automatedDrive", automatedDrive);
 
 
         //Operator controls
@@ -170,9 +164,79 @@ public class CombinedTeleOp extends OpMode {
             timer.reset();
             shooterSubsystem.stop();
         }
+
+//        //Automated PathFollowing
+//        if (gamepad1.aWasPressed()) {
+//            follower.followPath(pathChain.get());
+//            automatedDrive = true;
+//        }
+//        //Stop automated following if the follower is done
+//        if (automatedDrive && (gamepad1.bWasPressed() || !follower.isBusy())) {
+//            follower.startTeleopDrive();
+//            automatedDrive = false;
+//        }
+
+//        //Slow Mode
+//        if (gamepad1.rightBumperWasPressed()) {
+//            slowMode = !slowMode;
+//        }
+//
+//        //Optional way to change slow mode strength
+//        if (gamepad1.xWasPressed()) {
+//            slowModeMultiplier += 0.25;
+//        }
+//
+//        //Optional way to change slow mode strength
+//        if (gamepad2.yWasPressed()) {
+//            slowModeMultiplier -= 0.25;
+//        }
+
+        telemetryM.debug("position", follower.getPose());
+        telemetryM.debug("velocity", follower.getVelocity());
+        telemetryM.debug("automatedDrive", automatedDrive);
+
+
+
     }
+    private void driveToFarBlue(){
+        follower.followPath(
+                follower.pathBuilder()
+                        .addPath(new BezierLine(follower.getPose(), farBlue))
+                        .setLinearHeadingInterpolation(follower.getHeading(), farBlue.minus(follower.getPose()).getAsVector().getTheta())
+                        .build()
+        );
+    }
+
+    private void driveToFarRed(){
+        follower.followPath(
+                follower.pathBuilder()
+                        .addPath(new BezierLine(follower.getPose(), farRed))
+                        .setLinearHeadingInterpolation(follower.getHeading(), farRed.minus(follower.getPose()).getAsVector().getTheta())
+                        .build()
+        );
+    }
+
+    private void driveToNearBlue(){
+        follower.followPath(
+                follower.pathBuilder()
+                        .addPath(new BezierLine(follower.getPose(), nearBlue))
+                        .setLinearHeadingInterpolation(follower.getHeading(), nearBlue.minus(follower.getPose()).getAsVector().getTheta())
+                        .build()
+        );
+    }
+
+    private void driveToNearRed(){
+        follower.followPath(
+                follower.pathBuilder()
+                        .addPath(new BezierLine(follower.getPose(), nearRed))
+                        .setLinearHeadingInterpolation(follower.getHeading(), nearRed.minus(follower.getPose()).getAsVector().getTheta())
+                        .build()
+        );
+    }
+
     public void stop() {}
     }
+
 
 
 
